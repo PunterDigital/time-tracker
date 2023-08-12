29320bba0be275d73a431c7d3042f80ea1e2c4aa
ShayPunter <icodinqs@gmail.com>
2023-08-12 01:08:14 +0100

    fix(registration): add register link to login page

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -77,6 +77,10 @@ const submit = () => {
             </div>
 
             <div class="flex items-center justify-end mt-4">
+                <Link :href="route('register')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                  Not registered?
+                </Link>
+
                 <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Forgot your password?
                 </Link>
